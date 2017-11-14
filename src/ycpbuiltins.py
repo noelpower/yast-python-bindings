1f4bc1674f408c284284b084ce0be5364ce74e92
Noel Power <noel.power@suse.com>
2017-11-14 20:16:38 +0000

    Support for Term (to allow it to be constucted with native python types)

--- src/ycpbuiltins.py
+++ src/ycpbuiltins.py
@@ -1,18 +1,18 @@
-from yast import Term
+from ycp import List, String, Integer, Boolean, Float, Value
+from ycp import Term as YCPTerm
+
 import time
 
 # placeholder for proper logging function
-# currently just dumps to stderr (should 
+# currently just dumps to stderr (should
 # however write to yast log file)
 def y2milestone(msg):
     print msg
-
 # placeholder for proper logging function
-# currently just dumps to stderr (should 
+# currently just dumps to stderr (should
 # however write to yast log file)
 def y2error(msg):
     print msg
-
 # placeholder for proper logging function
 # currently just dumps to stderr (should 
 # however write to yast log file)
@@ -28,12 +28,35 @@ def foreach(listOrMap):
         return None
     return listOrMap
 
+def convert_to_ycp(pyvalue):
+    if isinstance(pyvalue, Value):
+        return pyvalue
+    elif isinstance(pyvalue, int):
+        return Integer(pyvalue)
+    elif isinstance(pyvalue, str):
+        return String(pyvalue)
+    elif isinstance(pyvalue, list):
+        l = List()
+        for item in key:
+            l.add(convert_to_ycp(item))
+        return l
+    elif isinstance(pyvalue, dict):
+        m = Map()
+        for key, value in pyvalue:
+            m.add(convert_to_ycp(key), convert_to_ycp(value))
+        return m
+    elif isinstance(pyvalue, float):
+        f = Float(pyvalue)
+        return f
+    raise NotImplementedError()
+
 # add — Add a key/value pair to a map or list
 def add(listOrMap, key, value=None):
     if isinstance(listOrMap, dict):
         listOrMap[key] = value
-    elif isinstance(listOrMap, Term):
-         listOrMap.add(key)
+    elif isinstance(listOrMap, List):
+         ycpvalue = convert_to_ycp(key)
+         listOrMap.add(ycpvalue)
     else: # assume list
         listOrMap.append(key)
     # should we clone the listOrMap here ?
